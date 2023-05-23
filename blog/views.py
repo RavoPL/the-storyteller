da81efc4bf66c0771c2d706598c36df208c88de7
RavoPL <ravosilesia88@protonmail.com>
2023-05-23 12:35:08 +0000

    add basic list display and custom titles

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,32 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
+blogposts = [
+    {
+        'author': 'Dorian',
+        'title': 'Lord of the Rings',
+        'description': 'There and Back Again',
+        'date_posted': 'May 23rd, 2023',
+    },
+    {
+        'author': 'Jack',
+        'title': 'Harry Potter',
+        'description': 'Youre a Wizard',
+        'date_posted': 'May 22nd, 2023',
+    },
+    {
+        'author': 'Mary',
+        'title': 'Lovecraft',
+        'description': 'Cosmic Horror',
+        'date_posted': 'May 10th, 2023',
+    },
+]
+
 def homepage(request):
-    return render(request, "blog/homepage.html")
+    context = {
+        'blogposts': blogposts
+    }
+    return render(request, "blog/homepage.html", context)
 
 def aboutpage(request):
-    return render(request, "blog/aboutpage.html")
+    return render(request, "blog/aboutpage.html", {'title': 'About Page'})
