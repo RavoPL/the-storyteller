282ace0ea24b463c4eab10b2177f1d5c851aae48
RavoPL <ravosilesia88@protonmail.com>
2023-05-23 12:11:59 +0000

    add urls and views

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def homepage(request):
-    return HttpResponse("<h1>TEST MAIN PAGE</h1>")
+    return render(request, "blog/homepage.html")
 
 def aboutpage(request):
     return HttpResponse("<h2>TEST ABOUT PAGE<h2>")
