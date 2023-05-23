f7c42c163e99f3084c523ec096fa500f0871c39c
RavoPL <ravosilesia88@protonmail.com>
2023-05-23 12:15:43 +0000

    add aboutpage template link

--- blog/views.py
+++ blog/views.py
@@ -6,4 +6,4 @@ def homepage(request):
     return render(request, "blog/homepage.html")
 
 def aboutpage(request):
-    return HttpResponse("<h2>TEST ABOUT PAGE<h2>")
+    return render(request, "blog/aboutpage.html")
