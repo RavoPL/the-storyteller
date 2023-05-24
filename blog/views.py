b7588c3b59ae8bab8a5fd9dc01140fe6eebf8379
RavoPL <ravosilesia88@protonmail.com>
2023-05-24 21:28:12 +0000

    add views import and homepage href

--- blog/views.py
+++ blog/views.py
@@ -1,29 +1,10 @@
 from django.shortcuts import render, HttpResponse
+from . import models
 
 # Create your views here.
 
-blogposts = [
-    {
-        'author': 'Dorian',
-        'title': 'Lord of the Rings',
-        'description': 'There and Back Again',
-        'date_posted': 'May 23rd, 2023',
-    },
-    {
-        'author': 'Jack',
-        'title': 'Harry Potter',
-        'description': 'Youre a Wizard',
-        'date_posted': 'May 22nd, 2023',
-    },
-    {
-        'author': 'Mary',
-        'title': 'Lovecraft',
-        'description': 'Cosmic Horror',
-        'date_posted': 'May 10th, 2023',
-    },
-]
-
 def homepage(request):
+    blogposts = models.Blogpost.objects.all()
     context = {
         'blogposts': blogposts
     }
