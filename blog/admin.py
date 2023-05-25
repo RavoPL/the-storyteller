a8e7c9e639867dc96d53c6fc8ae616e59d8f08f2
RavoPL <ravosilesia88@protonmail.com>
2023-05-25 08:53:10 +0000

    add admin comment functionality

--- blog/admin.py
+++ blog/admin.py
@@ -7,7 +7,7 @@ from django_summernote.admin import SummernoteModelAdmin
 class BlogAdmin(SummernoteModelAdmin):
     # Automatically populates the slug field with the value in the title field
     prepopulated_fields = {'slug': ('title',)}
-    # Allows the admin to filter through dates of comment and post creation
+    # Allows the admin to filter through dates of post creation
     list_filter = ('status', 'created_at')
     # Displays the title, slug, status and date of creation of a post
     list_display = ('title', 'slug', 'status', 'created_at')
@@ -15,3 +15,12 @@ class BlogAdmin(SummernoteModelAdmin):
     search_fields = ['title', 'content']
     # Enables Summernote features in the content section
     summernote_fields = ('content')
+
+@admin.register(models.Comment)
+class CommentAdmin(admin.ModelAdmin):
+    # Allows the admin to filter through dates and approval status of comment creation
+    list_filter = ('approved', 'created_at')
+    # Displays the user name, body of comment, date of creation and approval status
+    list_display = ('name', 'body', 'post', 'created_at', 'approved')
+    # Allows the admin to search for comments by name, user e-mail or body of comment
+    search_fields = ['name', 'email', 'body']
