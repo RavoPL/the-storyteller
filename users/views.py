5b3ec4dffee2d79cf09e61f9f0033e42df440e13
RavoPL <ravosilesia88@protonmail.com>
2023-05-25 09:39:03 +0000

    add acc registration post functionality

--- users/views.py
+++ users/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
 def register(request):
-    form = UserCreationForm()
+    # If the request is POST the form will be passed in
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        # If the form is valid the user will receive a prompt and be redirected to the homepage
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"{username}, your account has been created!")
+            return redirect('storyteller-homepage')
+        # If the form is NOT valid a registration form will be returned
+    else:
+        form = UserCreationForm()
     return render(request, 'users/registration.html', {'form': form})
