54a9ff9e980fbcba442c94a8bf559ab198e1ec0a
RavoPL <ravosilesia88@protonmail.com>
2023-11-29 18:40:01 +0000

    add post detail view

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.PostList.as_view(), name="home"),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     # path('about/', views.aboutpage, name="storyteller-aboutpage"),
     # path('contact/', views.contactpage, name="storyteller-contactpage"),
 ]
