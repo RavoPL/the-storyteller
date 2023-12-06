428b087b9886a54e532b6c9e377a60e36b1e10d1
RavoPL <ravosilesia88@protonmail.com>
2023-12-06 21:21:20 +0000

    add like functionality

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.PostList.as_view(), name="home"),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
     # path('about/', views.aboutpage, name="storyteller-aboutpage"),
     # path('contact/', views.contactpage, name="storyteller-contactpage"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, HttpResponse, get_object_or_404
-# from . import models
+from django.shortcuts import render, HttpResponse, get_object_or_404, reverse
+from django.http import HttpResponseRedirect
 from .models import Post
 from django.views import generic, View
 from .forms import CommentForm
@@ -63,6 +63,17 @@ class PostDetail(View):
                 "liked": liked
             },
         )
+
+class PostLike(View):
+    
+    def post(self, request, slug, *args, **kwargs):
+        post = get_object_or_404(Post, slug=slug)
+        if post.likes.filter(id=request.user.id).exists():
+            post.likes.remove(request.user)
+        else:
+            post.likes.add(request.user)
+
+        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
 
 # def homepage(request):
 #     blogposts = models.Blogpost.objects.all()
